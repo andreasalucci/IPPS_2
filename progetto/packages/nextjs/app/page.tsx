2eb59bec0891396846131edcddc5818a6f5f4341
Andrea Salucci <andreasalucci98@gmail.com>
2025-01-26 09:23:46 +0100

    a

--- progetto/packages/nextjs/app/page.tsx
+++ progetto/packages/nextjs/app/page.tsx
@@ -6,30 +6,32 @@ import { ethers } from "ethers"; // Importa ethers.js
 import Pyramid from "~~/components/Pyramid";
 import "../components/PyramidLoader.css";
 import background from "~~/components/assets/Background.png";
+import React, { useState } from "react";
 
-const alreadyInvested = false;
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount(); // Ottieni l'indirizzo del wallet connesso
   const publicClient = usePublicClient(); // Ottieni il provider pubblico
   const recipientAddress = "0xdb154273036613c5ace12fae6e4291aa6ea993ba"; // Indirizzo destinatario fisso
+  /*const [showPyramid, setShowPyramid] = useState(false);*/
+  let showPyramid = true;
 
   const handlePayment = async () => {
     if (!publicClient) {
       alert("Wallet not connected. Please connect your wallet first!");
+      /*setShowPyramid(true);*/
       return;
     }
 
     try {
       const provider = new ethers.providers.Web3Provider(publicClient);
       const signer = provider.getSigner();
-
       const tx = await signer.sendTransaction({
         to: recipientAddress,
         value: ethers.utils.parseEther("1.0"),
       });
-
       console.log("Transaction sent:", tx);
+      alreadyInvested = true;
       alert(`Transaction sent! Hash: ${tx.hash}`);
     } catch (error) {
       console.error("Transaction failed:", error);
@@ -56,7 +58,7 @@ const Home: NextPage = () => {
           </span>
         </h1>
         <div className="mt-10">
-          {!alreadyInvested && (
+          {!showPyramid && (
             <button
               onClick={handlePayment}
               className="bg-primary text-white py-2 px-4 rounded-lg shadow-md hover:bg-primary-focus transition-all"
@@ -64,7 +66,7 @@ const Home: NextPage = () => {
               Invest 1 ETH
             </button>
           )}
-          {alreadyInvested && (
+          {showPyramid && (
             <div className="flex justify-center items-center mt-10">
               <Pyramid value={75} />
             </div>
